fddbd20a43bd3fed0218a290a19ad068b76d0c26
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-06-01 11:43:40 -0300

    feat: extrai dados da api e faz tratativas para dados faltantes de finais de semana

--- src/extract.py
+++ src/extract.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import pandas as pd
+import logging
+from datetime import datetime
 
 
 class Extract():
@@ -8,13 +10,40 @@ class Extract():
         self.url = url
         
     def extract_api(self):
-        request = requests.get(self.url).json().get('value')
-        return request
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            data = response.json().get('value')
+            if data:
+                return data
+        except Exception as e:
+            logging.info(f'Erro ao extrair dados: {e}')
+            return []
     
-    def request_into_dataframe(self, request):
-        df = pd.DataFrame(request)
-        
+    def request_into_dataframe(self, response):
+        df = pd.DataFrame(response)
+        df['data_extracao'] = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         return df
     
     def save_dataframe_into_csv(self, df, name):
-        df.to_parquet(f'D:\\Projetos\\Python\\Data_Engineering\\dolar-pipeline\\data\\{name}.parquet', index=False)
+        df.to_csv(f'D:\\Projetos\\Python\\Data_Engineering\\dolar-pipeline\\data\\{name}.csv', index=False)
+    
+    def save_dataframe_into_parquet(self, df, name):
+        df.to_csv(f'D:\\Projetos\\Python\\Data_Engineering\\dolar-pipeline\\data\\{name}.parquet', index=False)
+        
+    def tratativas_dataframe(self, df):
+        df['dataHoraCotacao'] = pd.to_datetime(df['dataHoraCotacao'])
+        df['data'] = df['dataHoraCotacao'].dt.date
+        
+        datas = pd.date_range(start='2024-01-01', end='2025-05-15', freq='D')
+        df_datas = pd.DataFrame(datas, columns=['data'])
+        
+        df['data'] = pd.to_datetime(df['data'])         
+        df_datas['data'] = pd.to_datetime(df_datas['data'])
+        
+        df_merged = pd.merge(df_datas, df[['data', 'cotacaoCompra', 'cotacaoVenda', 'dataHoraCotacao', 'data_extracao']], on='data', how='left')
+        df_merged.sort_values('data', inplace=True)  
+        df_merged.fillna(method='bfill', inplace=True) 
+        df_merged.fillna(method='ffill', inplace=True)
+        
+        return df_merged
--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import logging
+
 from extract import Extract
 
 def create_extractor(url):
@@ -5,12 +8,25 @@ def create_extractor(url):
 
 
 if __name__ == '__main__':
-    url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1\
-    /odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?%40dataCotacao='05-30-2025'&%24format=json"
+    
+    logging.basicConfig(level=logging.INFO)
+    
+    data = datetime.now().strftime("%m-%d-%Y") 
+    horario = datetime.now().strftime("%H-%M")
+    file_name = f"dolar_{data}_{horario}"
+    
+    
+    url = f"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='01-01-2024'&@dataFinalCotacao='05-15-2025'&$top=500&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao"
     
     
     extractor = create_extractor(url)
-    request = extractor.extract_api()
-    dataframe = extractor.request_into_dataframe(request)
-    extractor.save_dataframe_into_csv(df=dataframe, name='dolar_1')
+    logging.info(f'Extraindo dados {data}')
+    
+    response = extractor.extract_api()
+    dataframe = extractor.request_into_dataframe(response)
+    extractor.save_dataframe_into_csv(df=dataframe, name=file_name)
+    ##extractor.save_dataframe_into_parquet(df=dataframe, name=file_name)
+    
+    dataframe_final = extractor.tratativas_dataframe(dataframe)
+    extractor.save_dataframe_into_csv(df=dataframe_final, name="final")
     
